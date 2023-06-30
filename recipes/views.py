11e2fa317938130453d352bfef57768d2151190f
Lucas Gomes <lucasceo22@gmail.com>
2023-06-30 20:50:41 -0300

    renomeando os arquivos

--- recipes/views.py
+++ recipes/views.py
@@ -5,9 +5,12 @@ from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(
-        is_published=True
-    ).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            is_published=True,
+        ).order_by('-id')
+    )
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
