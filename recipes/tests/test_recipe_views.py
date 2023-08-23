1fe6a5f5984ec99b2f2043283bab55a764640dec
Lucas Gomes <lucasceo22@gmail.com>
2023-08-23 10:37:55 -0300

    estando "No found" quqn#qreceitas não existem

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -24,3 +24,11 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            '<h1>No recipes found here😥</h1>',
+            response.content.decode('utf-8')
+        )
+    
